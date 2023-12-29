4ef87660c1be86eb51046cb11cea833abb70fb1e
Afraaz09 <afraazkhan.i2002@gmail.com>
2023-12-29 20:47:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    let getMode = localStorage.getItem("mode");
+
+    if(getMode == "dark"){
+        $( "<link rel='stylesheet' href='../dark-style.css' id='darkStyleSheet'>" ).insertAfter("#styleSheet");
+        $(".fa-moon").hide();
+        $(".fa-lightbulb").show();
+    }else{
+        $(".fa-moon").show();
+        $(".fa-lightbulb").hide();
+    }
+
+    $("#switch").click(() => {
+        if ($("#darkStyleSheet").length) {
+            $("#darkStyleSheet").remove();
+            localStorage.setItem("mode", "light");
+            $(".fa-moon").show();
+            $(".fa-lightbulb").hide();
+        }
+        else{
+            $( "<link rel='stylesheet' href='../dark-style.css' id='darkStyleSheet'>" ).insertAfter("#styleSheet");
+            localStorage.setItem("mode", "dark");
+            $(".fa-moon").hide();
+            $(".fa-lightbulb").show();
+        }
+    })
+
+    $(".fa-bars").click(function () {
+        $(".side-menu").addClass("open");
+    });
+    $(".fa-xmark").click(function () {
+        $(".side-menu").removeClass("open");
+    });
+
+});
+
